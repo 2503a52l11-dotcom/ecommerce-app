557665bc3bb34346f3fec518d7dc7025123ae74c
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:48:00 +0530

    Add shopping cart functionality

--- cart/cart.js
+++ cart/cart.js
@@ -1,9 +1,2 @@
 let cart = [];
-
-function addToCart(product) {
-    cart.push(product);
-}
-
-function removeFromCart(productName) {
-    cart = cart.filter(product => product.name !== productName);
-}
+function addToCart(product) { cart.push(product); }
