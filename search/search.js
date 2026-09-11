1265bd68e69c69ca1175262d02e08e33733a897b
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:46:53 +0530

    Add product price filtering

--- search/search.js
+++ search/search.js
@@ -1,1 +1,2 @@
 function searchProducts(products, keyword) { return products.filter(product => product.name.toLowerCase().includes(keyword.toLowerCase())); }
+function filterByPrice(products, maxPrice) { return products.filter(product => product.price <= maxPrice); }
