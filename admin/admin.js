93d5febc73d376e6988a3389831ba7806a88ce55
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:51:40 +0530

    Add admin order management

--- admin/admin.js
+++ admin/admin.js
@@ -1,2 +1,3 @@
 function showDashboard() { console.log("Admin Dashboard"); }
 function showProducts(products) { console.log("Products:", products); }
+function showOrders(orders) { console.log("Orders:", orders); }
