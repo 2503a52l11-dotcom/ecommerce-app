c92fc13fa5d20e3dba22994e75b5b34e28c1644f
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:48:01 +0530

    Add cart total calculation

--- cart/cart.js
+++ cart/cart.js
@@ -1,3 +1,4 @@
 let cart = [];
 function addToCart(product) { cart.push(product); }
 function removeFromCart(productName) { cart = cart.filter(product => product.name !== productName); }
+function calculateTotal() { return cart.reduce((total, product) => total + product.price, 0); }
