afe818f389058ec153e9b5b3451b5a1261a87533
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:51:39 +0530

    Add admin dashboard

--- admin/admin.js
+++ admin/admin.js
@@ -1,11 +1,2 @@
-function showDashboard() {
-    console.log("Admin Dashboard");
-}
-
-function showProducts(products) {
-    console.log("Products:", products);
-}
-
-function showOrders(orders) {
-    console.log("Orders:", orders);
-}
+function showDashboard() { console.log("Admin Dashboard"); }
+function showProducts(products) { console.log("Products:", products); }
