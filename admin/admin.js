263fc1825e89e15dd09b73a56051ad26cba5517d
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:51:40 +0530

    Add admin statistics

--- admin/admin.js
+++ admin/admin.js
@@ -1,3 +1,4 @@
 function showDashboard() { console.log("Admin Dashboard"); }
 function showProducts(products) { console.log("Products:", products); }
 function showOrders(orders) { console.log("Orders:", orders); }
+function showStatistics(products, orders) { console.log("Total Products:", products.length); console.log("Total Orders:", orders.length); }
