d31e957450c8e591ee6e4ad08e60a6a550f87aef
2503a52l11-dotcom <2503a52l11@sru.edu.in>
2026-09-11 12:49:37 +0530

    Add payment processing functionality

--- payment/payment.js
+++ payment/payment.js
@@ -1,13 +1,1 @@
-function processPayment(amount) {
-    if (amount > 0) {
-        return {
-            success: true,
-            message: "Payment successful"
-        };
-    }
-
-    return {
-        success: false,
-        message: "Invalid amount"
-    };
-}
+function processPayment(amount) { if (amount > 0) { return { success: true, message: "Payment successful" }; } return { success: false, message: "Invalid amount" }; }
